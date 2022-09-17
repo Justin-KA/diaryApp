28c09e058c640ad2666a83c614507b5c1b6be280
Justin KA <justinalexander101@gmail.com>
2022-09-16 22:38:38 -0400

    google profile received

---
+++ config/passport.js
@@ -0,0 +1,26 @@
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+const mongoose = require('mongoose')
+const User = require('../models/User')
+
+module.exports = function(passport) {
+    passport.use(new GoogleStrategy( {
+        clientID: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        callbackURL: '/auth/google/callback'
+    },
+    async (accessToken, refreshToken, profile, done) => {
+        console.log(profile)
+    }))
+
+    // used to serialize the user for the session
+    passport.serializeUser(function(user, done) {
+        done(null, user.id); 
+    });
+
+    // used to deserialize the user
+    passport.deserializeUser(function(id, done) {
+        User.findById(id, function(err, user) {
+            done(err, user);
+        });
+    });
+}
